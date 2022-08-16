96f3e9e758ceef5059f59480013491fccd3b9bcb
alexandr <alex98saratovtsev@gmail.com>
2022-08-16 20:25:50 +0400

    added all classwork4

---
+++ Task24/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 */
+Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0; // default
+
+// for (int i = 1; i <= num; i++)
+// {
+//     sum += i; // sum = sum + i;   
+// }
+
+// Console.WriteLine($"Сумма чисел от 1 до {num}: {sum}");
+
+int SumNumber(int number)
+{
+    int sum = 0; // default
+    for (int i = 1; i <= num; i++)
+    {
+    sum += i; // sum = sum + i;   
+    }
+    return sum;
+}
+
+int result = SumNumber(num);
+Console.WriteLine($"Сумма чисел от 1 до {num}: {result}");
---
+++ Task30/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(0, 2);
+//         index++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(col[position]);
+//         position++;
+//     }
+// }
+
+// int[] array = new int[8];
+
+// FillArray(array);
+// PrintArray(array);
+
+void MasRandom(int[] array)
+{
+    Random rmd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rmd.Next(0, 2);
+    }
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i < array.Length-1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Вывод массива:");
+int[] mas = new int[8];
+MasRandom(mas);
+ShowArray(mas);
