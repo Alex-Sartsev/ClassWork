78f0f51645f1f33459a7a447a76512e0c6c5e858
alexandr <alex98saratovtsev@gmail.com>
2022-08-25 23:55:04 +0400

    Added ClassWork7

---
+++ Task42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = 0;
+int mult = 1;
+while(num > 0)
+{
+    result += num % 2 * mult;
+    num = num / 2;
+    mult *= 10;
+}
+
+Console.WriteLine(result);
+
+// Рекурсия:
+// int number = 13;
+
+// void DecToBin(int num)
+// {
+//     if(num == 0) return;
+//     DecToBin(num / 2);
+//     Console.Write(num % 2); 
+// }
+
+// DecToBin(number);
---
+++ Task51/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max) 
+{
+    
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+} 
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(j < matrix.GetLength(1) - 1 ? 
+            $"{matrix[i,j], 4}," : $"{matrix[i,j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int MatrixSumIdx(int[,] matrix) 
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(1) && i < matrix.GetLength(0); i++) // строки
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9);
+PrintMatrix(array2D);
+Console.WriteLine($"Сумма чисел диагонали: {MatrixSumIdx(array2D)}");
